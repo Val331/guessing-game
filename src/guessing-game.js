2e4b75e6a816cf936f037b619cf6e29d2adee232
Valera Mamrovsky <valeramamrovsky@gmail.com>
2023-01-19 21:45:45 +0300

    feat: answer task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = Math.ceil((this.min + this.max) / 2);
+        return this.value;
     }
 
     lower() {
-
+        this.setRange(this.min, this.value);
     }
 
     greater() {
-
+        this.setRange(this.value, this.max);
     }
+    
 }
 
 module.exports = GuessingGame;
